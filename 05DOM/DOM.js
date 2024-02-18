07fa92c9ad4eb477b7ae8fd4c01da5a93fac49e7
Satvick Pathak <117937493+Satvickk@users.noreply.github.com>
2024-02-18 14:17:05 +0000

    .

---
+++ 05DOM/DOM.js
@@ -0,0 +1,21 @@
+// Document object model is a tree like structure of html 
+
+// we have browser -> window object -> document -> html -> head and body
+
+// everything in js can be refered as node like : text-node, body node, head node, element node, etc
+
+// by referring to index.html we observe that : -
+
+/*
+
+Head ->  (meta tag and) ,title -> text-node(My first website)
+
+meta tag has attribute 
+
+Body -> div(it has attribute) -> <h1> and <p>
+        -> <h1> (has attribute ''text-white) -> text-node
+        -> <p> (has no attribute) -> text-node
+
+
+
+*/
