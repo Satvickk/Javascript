d1a1df3ff6eb78ffd81c381385adb6a7c0cd9c64
Satvick Pathak <117937493+Satvickk@users.noreply.github.com>
2024-03-03 15:56:39 +0000

    completed call

--- 09classesAndOOPS/04call.js
+++ 09classesAndOOPS/04call.js
@@ -6,10 +6,13 @@ function setUser(username) {
 function createUser(username, password, email) {
     this.password = password;
     this.email = email
-    setUser.call(this, username)             // here we are passing the current context of function createUser to setUSer with call method
+    // setUser(username)             // here it looks like we are calling the Function in real we are just passing the reference
+    setUser.call(this, username)             // here we are passing the current context 'this' (optional) of function createUser to setUSer with call method ()
     
 }
 
-// we use call because in the call stack the setUser will be called , executed and deleted from memory thus we will not have the username this we use call in which the current context is passed so that the value is set and even after the function setUser is removed from the memory we will have the value 
+// we use call because in the call stack the setUser will be called , executed and freed from memory thus we will not have the username, thus we use call in which the current context 'this' of createUser is passed to setUser so that the value of username will be set to the createUser's context 'this' and even after the function setUser is removed from the memory we will have the value of username in createUser's this
 
-console.log(new createUser("satvick", "123", 'satvick@gmail.com'));                     // dont forget to use new keyword
+
+const userOne = new createUser("satvick", "123", 'satvick@gmail.com')
+console.log(userOne);                     // dont forget to use new keyword
